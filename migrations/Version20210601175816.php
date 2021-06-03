6e120f4cc741e4bd985991f2f9cc523ea27cbfbf
Abdel93000 <74414343+Abdel93000@users.noreply.github.com>
2021-06-03 11:48:43 +0200

    rajout dossier projet

---
+++ migrations/Version20210601175816.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210601175816 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE role');
+        $this->addSql('ALTER TABLE roles ADD role VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user ADD roles_id INT NULL');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D64938C751C4 FOREIGN KEY (roles_id) REFERENCES roles (id)');
+        $this->addSql('CREATE INDEX IDX_8D93D64938C751C4 ON user (roles_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE role (id INT AUTO_INCREMENT NOT NULL, role VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE roles DROP role');
+        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D64938C751C4');
+        $this->addSql('DROP INDEX IDX_8D93D64938C751C4 ON user');
+        $this->addSql('ALTER TABLE user DROP roles_id');
+    }
+}
